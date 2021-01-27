00184a493ad56ec2ed6278a7df10c74ac2067c2d
yuanjk <yuanjk@glodon.com>
2021-01-27 17:38:17 +0800

    add cim util module

---
+++ cim-util/src/main/java/com/yuanjk/tool/cim/IotEquipmentAndTreeRelation.java
@@ -0,0 +1,73 @@
+package com.yuanjk.tool.cim;
+
+import com.glodon.pcop.cim.engine.dataServiceEngine.dataMart.DimensionType;
+import com.glodon.pcop.cim.engine.dataServiceEngine.dataMart.Fact;
+import com.glodon.pcop.cim.engine.dataServiceEngine.dataMart.Relation;
+import com.glodon.pcop.cim.engine.dataServiceEngine.dataMart.RelationType;
+import com.glodon.pcop.cim.engine.dataServiceEngine.dataServiceBureau.CimDataSpace;
+import com.glodon.pcop.cim.engine.dataServiceEngine.dataWarehouse.ExploreParameters;
+import com.glodon.pcop.cim.engine.dataServiceEngine.util.exception.CimDataEngineDataMartException;
+import com.glodon.pcop.cim.engine.dataServiceEngine.util.exception.CimDataEngineInfoExploreException;
+import com.glodon.pcop.cim.engine.dataServiceEngine.util.exception.CimDataEngineRuntimeException;
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class IotEquipmentAndTreeRelation {
+    private static final Logger log = LoggerFactory.getLogger(IotEquipmentAndTreeRelation.class);
+
+    public static boolean addRelationType(CimDataSpace cds, String relationTypeId) throws CimDataEngineDataMartException {
+        if (cds.hasRelationType(relationTypeId)) {
+            log.info("relation type of [{}] is already exists!", relationTypeId);
+            return true;
+        } else {
+            RelationType relationType = cds.addRelationType(relationTypeId);
+            if (relationType != null) {
+                return true;
+            } else {
+                log.error("create relation type of [{}] failed!", relationTypeId);
+                return false;
+            }
+        }
+    }
+
+    public static void createRelation(CimDataSpace cds, String sourceTypeId, String targetTypeId, String relationType)
+            throws CimDataEngineRuntimeException, CimDataEngineInfoExploreException {
+        ExploreParameters ep = new ExploreParameters();
+        ep.setResultNumber(Integer.MAX_VALUE);
+        ep.setType(sourceTypeId);
+
+        List<Fact> sourceFacts = cds.getInformationExplorer().discoverInheritFacts(ep);
+        if (CollectionUtils.isNotEmpty(sourceFacts)) {
+            int sourceFactSize = sourceFacts.size();
+            ep.setResultNumber(sourceFactSize);
+            ep.setType(targetTypeId);
+
+            List<Fact> targetFacts = cds.getInformationExplorer().discoverInheritFacts(ep);
+
+            if (CollectionUtils.isNotEmpty(targetFacts)) {
+                int targetFactSize = targetFacts.size();
+                for (int i = 0; i < sourceFactSize; i++) {
+                    Fact sourceFact = sourceFacts.get(i);
+                    Fact targetFact = targetFacts.get(i % targetFactSize);
+                    Relation relation = sourceFact.addToRelation(targetFact, relationType);
+                    if (relation != null) {
+                        log.info("one relation between facts is created: [{} -> {}]", sourceFact.getId(),
+                                targetFact.getId());
+                    } else {
+                        log.info("one relation between facts add failed: [{} -> {}]", sourceFact.getId(),
+                                targetFact.getId());
+                    }
+                }
+            } else {
+                log.error("no fact of target type [{}] is found", targetTypeId);
+            }
+        } else {
+            log.error("no fact of source type [{}] is found", sourceTypeId);
+        }
+    }
+
+
+}
---
+++ cim-util/src/test/java/com/yuanjk/tool/cim/IotEquipmentAndTreeRelationTest.java
@@ -0,0 +1,53 @@
+package com.yuanjk.tool.cim;
+
+import com.glodon.pcop.cim.engine.dataServiceEngine.dataServiceBureau.CimDataSpace;
+import com.glodon.pcop.cim.engine.dataServiceEngine.util.config.PropertyHandler;
+import com.glodon.pcop.cim.engine.dataServiceEngine.util.exception.CimDataEngineDataMartException;
+import com.glodon.pcop.cim.engine.dataServiceEngine.util.exception.CimDataEngineInfoExploreException;
+import com.glodon.pcop.cim.engine.dataServiceEngine.util.exception.CimDataEngineRuntimeException;
+import com.glodon.pcop.cim.engine.dataServiceEngine.util.factory.CimDataEngineComponentFactory;
+import com.glodon.pcop.cim.engine.dataServiceFeature.util.OrientdbConfigUtil;
+import junit.framework.TestCase;
+
+public class IotEquipmentAndTreeRelationTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+        PropertyHandler.map = OrientdbConfigUtil.getParameters();
+        PropertyHandler.map.put(PropertyHandler.DISCOVER_ENGINE_SERVICE_LOCATION, "remote:39.106.23.94:11452/");
+//        PropertyHandler.map.put(PropertyHandler.DISCOVER_ENGINE_SERVICE_LOCATION, "remote:localhost/");
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testAddRelationType() throws CimDataEngineDataMartException {
+        String spaceName = "gyd";
+//        String spaceName = "gdc";
+        String relationType = "physical_equipment_and_tree_relation_type";
+        CimDataSpace cds = CimDataEngineComponentFactory.connectInfoDiscoverSpace(spaceName);
+        try {
+            IotEquipmentAndTreeRelation.addRelationType(cds, relationType);
+        } finally {
+            if (cds != null) {
+                cds.closeSpace();
+            }
+        }
+    }
+
+    public void testCreateRelation() throws CimDataEngineRuntimeException, CimDataEngineInfoExploreException {
+        String sourceType = "PhysicEquipment";
+        String targetType = "singleTree";
+        String spaceName = "gyd";
+        String relationType = "physical_equipment_and_tree_relation_type";
+        CimDataSpace cds = CimDataEngineComponentFactory.connectInfoDiscoverSpace(spaceName);
+        try {
+            IotEquipmentAndTreeRelation.createRelation(cds, sourceType, targetType, relationType);
+        } finally {
+            if (cds != null) {
+                cds.closeSpace();
+            }
+        }
+    }
+}
