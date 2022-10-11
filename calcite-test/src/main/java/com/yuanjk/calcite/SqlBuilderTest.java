3194522bf4941feca29b3674f51f76e6f093e447
jiankuan <272228741@qq.com>
2022-10-11 14:45:57 +0800

    add calcite test module

---
+++ calcite-test/src/main/java/com/yuanjk/calcite/SqlBuilderTest.java
@@ -0,0 +1,26 @@
+package com.yuanjk.calcite;
+
+import org.apache.calcite.sql.SqlDialect;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.util.SqlBuilder;
+
+/**
+ * 类<code>Doc</code>用于：TODO
+ *
+ * @author yuanjk
+ * @version 1.0
+ */
+public class SqlBuilderTest {
+
+    public static void main(String[] args) {
+
+        SqlBuilder sqlBuilder = new SqlBuilder(new SqlDialect(SqlDialect.EMPTY_CONTEXT));
+
+//        sqlBuilder.
+
+        SqlParser sqlParser = new S
+
+    }
+
+}
